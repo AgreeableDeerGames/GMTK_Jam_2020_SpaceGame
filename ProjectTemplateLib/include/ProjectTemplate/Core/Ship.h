2a4f4f44be2f2d1b4c86c81d9074814348c3edd8
Ryan Lavin <lavinrp@gmail.com>
2020-07-11 23:23:16 -0400

    MORE STATS

--- ProjectTemplateLib/include/ProjectTemplate/Core/Ship.h
+++ ProjectTemplateLib/include/ProjectTemplate/Core/Ship.h
@@ -3,6 +3,7 @@
 #include <ProjectTemplate/Utils/DllUtils.h>
 
 #include <GameBackbone/Core/Updatable.h>
+#include <GameBackbone/Util/RandGen.h>
 
 #include <map>
 #include <string>
@@ -31,9 +32,21 @@ namespace PT
             // 0% - 100% of Ship.
             fires,
 
-            // Water Reserve - Puts Out Fires
+            // Water Reserve - Puts Out Fires. Highly radioactive.
             // 0% to 100%. No lose state here, just a resource. Maybe slowly regenerates.
-            water
+            water,
+
+            // Nanites - Repair hull damage have chance to explode and cause fire
+            // 0% to 100%. Represents active nanites. No lose state here, just a resource. Maybe slowly regenerates.
+            nanites,
+
+            // Radiation - Kills living things
+            // 0% - 100%. 100% is lose state
+            radiation,
+
+            // Radiation Eating Bacteria - Reduces radiation. Eats hull too. Slowly die off on their own.
+            // 0% to 100%. No lose state here, just a resource. Maybe slowly regenerates.
+            bacteria
         };
 
         std::map<Stat, double> m_stats;
@@ -41,22 +54,32 @@ namespace PT
         Ship();
 
         void update(sf::Int64 elapsedTime) override;
-        std::string GetStatName(Ship::Stat);
+        std::string GetStatName(Ship::Stat) const;
 
         bool m_isHullBeingRepaired;
         bool m_isHeatingOn;
         bool m_isCoolingOn;
         bool m_areSprinklersOn;
+        bool m_releasingNanites;
+        bool m_isReleasingBacteria;
 
         void PrintToTerminal() const;
 
+        void UpdateShipStat(Stat stat, double change);
 
     private:
+
+        void UpdateShipStat(sf::Int64 elapsedTime, Stat stat, double change);
 
         void UpdateWater(sf::Int64 elapsedTime);
         void UpdateFire(sf::Int64 elapsedTime);
         void UpdateOxygen(sf::Int64 elapsedTime);
         void UpdateTemperature(sf::Int64 elapsedTime);
+        void UpdateNanites(sf::Int64 elapsedTime);
+        void UpdateRadiation(sf::Int64 elapsedTime);
+        void UpdateBacteria(sf::Int64 elapsedTime);
+        void UpdateHull(sf::Int64 elapsedTime);
 
+        GB::RandGen m_randGen;
     };
 }
--- ProjectTemplateLib/source/Core/Ship.cpp
+++ ProjectTemplateLib/source/Core/Ship.cpp
@@ -5,23 +5,40 @@
 
 using namespace PT;
 
-double oxygenGainedFromLifeSupport = 0.1;
-double oxygenLostFromHoles = 0.05;
-double oxygenLostFromFires = 0.0005;
+namespace 
+{
+    double oxygenGainedFromLifeSupport = 0.1;
+    double oxygenLostFromHoles = 0.05;
+    double oxygenLostFromFires = 0.0005;
 
-double hullLostFromDamage = 0.1;
-double hullGainedFromRepairs = 0.5;
+    double hullLostFromDamage = 0.1;
+    double hullGainedFromRepairs = 0.5;
 
-double heatGainedFromFires = 0.05;
-double heatLostFromHoles = 0.05;
-double heatGainedFromHeating = 0.5;
-double heatLostFromCooling = 0.5;
+    double heatGainedFromFires = 0.05;
+    double heatLostFromHoles = 0.05;
+    double heatGainedFromHeating = 0.5;
+    double heatLostFromCooling = 0.5;
 
-double fireSpread = 0.1;
-double firePutOutBySprinklers = 0.5;
+    double fireSpread = 0.1;
+    double firePutOutBySprinklers = 0.5;
 
-double waterGenerated = 0.1;
-double waterLostBySprinklers = 0.5;
+    double waterGenerated = 0.1;
+    double waterLostBySprinklers = 0.5;
+    double radiationGainedFromSprinklers = 0.2;
+
+    double hullRepairedByNanites = 0.01;
+    double naniteFireChance = 0.0001;
+    double naniteDeathChance = 0.001;
+    double nanitesReleased = 0.7;
+    double nanitesDestroyedPerDeath = 0.1;
+    double fireFromNaniteExplosion = 0.1;
+
+    double radiationEatenByBacteria = 0.5;
+    double hullEatenByBacteria = 0.1;
+    double bacteriaReleased = 0.4;
+    double bacteriaDeathRate = 0.1;
+}
+
 
 Ship::Ship() : 
     m_stats({ 
@@ -29,11 +46,17 @@ Ship::Ship() :
         {Stat::hullIntegrity, 100.0},
         {Stat::temperature, 50.0},
         {Stat::fires, 0.0},
-        {Stat::water, 100.0}}),
+        {Stat::water, 100.0},
+        {Stat::nanites, 0.0},
+        {Stat::radiation, 0.0}}),
+
     m_isHullBeingRepaired(false),
     m_isHeatingOn(false),
     m_isCoolingOn(false),
-    m_areSprinklersOn(false)
+    m_areSprinklersOn(false),
+    m_releasingNanites(false),
+    m_isReleasingBacteria(false),
+    m_randGen()
 {
 }
 
@@ -51,70 +74,97 @@ void Ship::update(sf::Int64 elapsedTime)
 
 void Ship::PrintToTerminal() const
 {
-    std::cout << m_stats.at(Stat::oxygen) << std::endl;
-    std::cout << m_stats.at(Stat::hullIntegrity) << std::endl;
-    std::cout << m_stats.at(Stat::temperature) << std::endl;
-    std::cout << m_stats.at(Stat::fires) << std::endl;
-    std::cout << m_stats.at(Stat::water) << std::endl;
+    for (auto& [key, value] : m_stats)
+    {
+        std::cout << GetStatName(key) << " " << value << std::endl;
+    }
 }
 
+void Ship::UpdateShipStat(Stat stat, double change)
+{
+    m_stats[stat] += change;
+    if (m_stats[stat] < 0)
+    {
+        m_stats[stat] = 0;
+    }
+    if (m_stats[stat] > 100)
+    {
+        m_stats[stat] = 100;
+    }
+}
+
+void PT::Ship::UpdateShipStat(sf::Int64 elapsedTime, Stat stat, double change)
+{
+    UpdateShipStat(stat, change * elapsedTime * 0.000001);
+}
 
 void Ship::UpdateWater(sf::Int64 elapsedTime)
 {
-    const double waterGeneratedPerSecond = waterGenerated * elapsedTime * 0.000001;
-    const double waterLostPerSecond = (int)m_areSprinklersOn * waterLostBySprinklers * elapsedTime * 0.000001;
-
-
-    m_stats[Stat::water] = m_stats[Stat::water] + waterGeneratedPerSecond - waterLostPerSecond;
-    if (m_stats[Stat::water] < 0)
-    {
-        m_stats[Stat::water] = 0;
-	}
-    if (m_stats[Stat::water] > 100)
-    {
-        m_stats[Stat::water] = 100;
-	}
+    const double gain = waterGenerated;
+    const double loss = (int)m_areSprinklersOn * waterLostBySprinklers;
+    UpdateShipStat(elapsedTime, Stat::water, gain - loss);
 }
 
 void Ship::UpdateFire(sf::Int64 elapsedTime)
 {
-    const double fireSpreadPerSecond = m_areSprinklersOn ? 0.0 : m_stats[Stat::fires] * fireSpread * elapsedTime * 0.000001;
-    const double firePutOutPerSecond = (int)m_areSprinklersOn * firePutOutBySprinklers * elapsedTime * 0.000001;
-
-    m_stats[Stat::fires] = m_stats[Stat::fires] + fireSpreadPerSecond - firePutOutPerSecond;
-    if (m_stats[Stat::fires] < 0)
-    {
-        m_stats[Stat::fires] = 0;
-	}
-    if (m_stats[Stat::fires] > 100)
-    {
-        m_stats[Stat::fires] = 100;
-	}
+    const double gain = m_areSprinklersOn ? 0.0 : m_stats[Stat::fires] * fireSpread;
+    const double loss = (int)m_areSprinklersOn * firePutOutBySprinklers;
+    UpdateShipStat(elapsedTime, Stat::fires, gain - loss);
 }
 
 void Ship::UpdateOxygen(sf::Int64 elapsedTime)
 {
-    const double oxygenGainedPerSecond = oxygenGainedFromLifeSupport * elapsedTime * 0.000001;
-    const double oxygenLostPerSecond = m_stats[Stat::oxygen] * m_stats[Stat::fires] * oxygenLostFromFires * elapsedTime * 0.000001;
-
-    m_stats[Stat::oxygen] = m_stats[Stat::oxygen] + oxygenGainedPerSecond - oxygenLostPerSecond;
-    //m_stats[Stat::oxygen] = m_stats[Stat::oxygen] + oxygenGainedFromLifeSupport - (oxygenLostFromHoles * m_stats[Stat::hullIntegrity] * m_stats[Stat::oxygen]) - (oxygenLostFromFires * m_stats[Stat::fires] * m_stats[Stat::oxygen]);
-    if (m_stats[Stat::oxygen] < 0)
-    {
-        m_stats[Stat::oxygen] = 0;
-	}
-    if (m_stats[Stat::oxygen] > 100)
-    {
-        m_stats[Stat::oxygen] = 100;
-	}
+    const double gain = oxygenGainedFromLifeSupport;
+    const double loss = m_stats[Stat::oxygen] * m_stats[Stat::fires] * oxygenLostFromFires;
+    UpdateShipStat(elapsedTime, Stat::oxygen, gain - loss);
 }
 
 void Ship::UpdateTemperature(sf::Int64 elapsedTime)
 {
-    m_stats[Stat::temperature] = m_stats[Stat::temperature] + (heatGainedFromFires * m_stats[Stat::fires]) - (heatLostFromHoles * m_stats[Stat::hullIntegrity]) + ((int)m_isHeatingOn * heatGainedFromHeating) - ((int)m_isCoolingOn * heatLostFromCooling);
+
+    const double gain = (heatGainedFromFires * m_stats[Stat::fires]);
+    const double loss = (heatLostFromHoles * m_stats[Stat::hullIntegrity]) + ((int)m_isHeatingOn * heatGainedFromHeating) - ((int)m_isCoolingOn * heatLostFromCooling);
+    UpdateShipStat(elapsedTime, Stat::temperature, gain - loss);
 }
 
-std::string Ship::GetStatName(Ship::Stat stat)
+void Ship::UpdateNanites(sf::Int64 elapsedTime)
+{
+    // TODO: MAKE THIS WORK
+    const double gain = (int)m_releasingNanites * nanitesReleased;
+
+    bool deadNanite = m_randGen.uniDist(0.0, 1.0) > naniteDeathChance * elapsedTime * 0.000001;
+    bool explodedNanite = m_randGen.uniDist(0.0, 1.0) > naniteFireChance * elapsedTime * 0.000001;
+    const double loss = (int)deadNanite * nanitesDestroyedPerDeath + int(explodedNanite) * nanitesDestroyedPerDeath;
+    UpdateShipStat(elapsedTime, Stat::nanites, gain - loss);
+
+    if (explodedNanite)
+    {
+        UpdateShipStat(Stat::fires, fireFromNaniteExplosion);
+    }
+}
+
+void Ship::UpdateRadiation(sf::Int64 elapsedTime)
+{
+    const double gain = (int)m_areSprinklersOn * radiationGainedFromSprinklers;
+    const double loss = m_stats[Stat::bacteria] * radiationEatenByBacteria;
+    UpdateShipStat(elapsedTime, Stat::radiation, gain - loss);
+}
+
+void Ship::UpdateBacteria(sf::Int64 elapsedTime)
+{
+    const double gain = (int)m_isReleasingBacteria * bacteriaReleased;
+    const double loss = bacteriaDeathRate;
+    UpdateShipStat(elapsedTime, Stat::bacteria, gain - loss);
+}
+
+void Ship::UpdateHull(sf::Int64 elapsedTime)
+{
+    const double gain = m_stats[Stat::nanites] * hullRepairedByNanites + (int)m_isHullBeingRepaired * hullGainedFromRepairs;
+    const double loss = m_stats[Stat::bacteria] * hullEatenByBacteria;
+    UpdateShipStat(elapsedTime, Stat::hullIntegrity, gain - loss);
+}
+
+std::string Ship::GetStatName(Ship::Stat stat) const
 {
 	static std::unordered_map<Ship::Stat, std::string> statMapping
 	{
@@ -122,7 +172,10 @@ std::string Ship::GetStatName(Ship::Stat stat)
 		{ Ship::Stat::hullIntegrity, "Hull Integrity" },
 		{ Ship::Stat::temperature, "Temperature" },
 		{ Ship::Stat::fires, "Fire" },
-		{ Ship::Stat::water, "Water Reservers" }
+        { Ship::Stat::water, "Water Reservers" },
+        { Ship::Stat::nanites, "Active Nanites" },
+        { Ship::Stat::radiation, "Radiation" },
+        { Ship::Stat::bacteria, "Released Bacteria" }
 	};
 
     return statMapping[stat];
