a8760679faeefd27c0eac28f0afef90c146d00df
Ryan Lavin <lavinrp@gmail.com>
2020-07-12 02:45:22 -0400

    initial work on generic terminal

---
+++ ProjectTemplateLib/include/ProjectTemplate/Core/ShipControlTerminalRegion.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <ProjectTemplate/Utils/DllUtils.h>
+#include <ProjectTemplate/Core/TerminalRegion.h>
+#include <ProjectTemplate/Core/Ship.h>
+
+#include <memory>
+#include <map>
+#include <string>
+#include <functional>
+
+namespace PT
+{
+	class libProjectTemplate ShipControlTerminalRegion : public PT::TerminalRegion
+	{
+	private:
+		ShipControlTerminalRegion(
+			sf::RenderWindow& window,
+			std::shared_ptr<Ship> ship,
+			std::vector<Ship::Stat> trackedStats,
+			std::shared_ptr<DataPad> dataPad,
+			std::unordered_map<std::string, std::function<void()>> availableFunctions,
+			std::string terminalName);
+
+		void update(sf::Int64 elapsedTime) override;
+
+	public:
+		const std::unordered_map<std::string, std::function<void()>> m_availableFunctions;
+		std::unordered_map<std::string, std::function<void()>>::const_iterator m_recordFunctionIterator;
+		std::string m_name;
+		bool m_isInitialized;
+		bool m_hasLoggedIn;
+
+	};
+}
---
+++ ProjectTemplateLib/source/Core/ShipControlTerminalRegion.cpp
@@ -0,0 +1,80 @@
+#include <ProjectTemplate/Core/ShipControlTerminalRegion.h>
+
+using namespace PT;
+
+ShipControlTerminalRegion::ShipControlTerminalRegion(
+	sf::RenderWindow& window,
+	std::shared_ptr<Ship> ship,
+	std::vector<Ship::Stat> trackedStats,
+	std::shared_ptr<DataPad> dataPad,
+	std::unordered_map<std::string, std::function<void()>> availableFunctions,
+	std::string terminalName) :
+
+	TerminalRegion(window, std::move(ship), std::move(trackedStats), std::move(dataPad)),
+	m_availableFunctions(std::move(availableFunctions)),
+	m_recordFunctionIterator(m_availableFunctions.begin()),
+	m_name(std::move(terminalName)),
+	m_isInitialized(false),
+	m_hasLoggedIn(false)
+{
+}
+
+void ShipControlTerminalRegion::update(sf::Int64 elapsedTime)
+{
+	TerminalRegion::update(elapsedTime);
+	
+	if (m_terminal.IsLoggedIn())
+	{
+		m_hasLoggedIn = true;
+		m_terminal.m_displayedTerminal->setText(m_name);
+		m_terminal.m_displayedTerminal->addText("\n\n");
+
+		if (!m_isInitialized)
+		{
+			// record Any available binds
+			if (m_recordFunctionIterator != m_availableFunctions.end())
+			{
+				const NumberGestureBind* bind = m_dataPad->GetBindWithName(m_recordFunctionIterator->first);
+				if (bind == nullptr)
+				{
+					m_terminal.m_displayedTerminal->addText("Recording data pad binding for " + m_recordFunctionIterator->first);
+					m_isInRecordState = true;
+					m_nextActionNameToBind = m_recordFunctionIterator->first;
+					m_nextActionToBind = m_recordFunctionIterator->second;
+				}
+				else
+				{
+					++m_recordFunctionIterator;
+				}
+
+			}
+			else
+			{
+				// We have recorded all available binds
+				m_isInitialized = true;
+			}
+		}
+		else
+		{
+			m_terminal.m_displayedTerminal->addText("Welcome back!");
+			m_terminal.m_displayedTerminal->addText("All available controls have already been bound to your data pad!");
+		}
+	}
+	else
+	{
+		m_terminal.m_displayedTerminal->setText(m_name);
+		m_terminal.m_displayedTerminal->addText("\n\n");
+		if (!m_hasLoggedIn)
+		{
+			// Give user login info
+			m_terminal.m_displayedTerminal->addText("New user identified");
+			m_terminal.m_displayedTerminal->addText("Your new password is: ");
+			m_terminal.m_displayedTerminal->addText(StringifyGesture(m_terminal.GetPasscode()));
+		}
+		else
+		{
+			m_terminal.m_displayedTerminal->addText("Please log in");
+		}
+	}
+
+}
